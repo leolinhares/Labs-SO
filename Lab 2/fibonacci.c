6fc39e97037a4360b161b615a8b8c833ec7ba618
Leo Linhares <slinharesleo@gmail.com>
2015-11-22 18:54:22 -0300

    fibonacci working as asked by the exercise

--- Lab 2/fibonacci.c
+++ Lab 2/fibonacci.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define FIB_QUANTITY 60
+#include <math.h>
 
-long double * fibonacci(long double final){
+
+/* Calculates the k th fibonacci term */
+long double fib(long double k){
+    return (1/sqrt(5))*( pow(((1+sqrt(5))/2),k) - pow(((1-sqrt(5))/2),k) );
+}
+
+
+/* Generates a fibonacci sequence from an initial value ini to a final value end */
+long double * fibonacci(long double ini, long double end){
     
-    long double first = 0, second = 1;
-    long double *fibArray = (long double *) malloc(final*(sizeof(long double)));
-    fibArray[0] = first;
-    fibArray[1] = second;
+    long double array_size = end-ini+1;
+    long double *array_fib = (long double *) malloc(array_size*(sizeof(long double)));
     
-    for (int counter = first; counter <= final; counter++) {
-        
-        long double next =  first + second;
-        first = second;
-        second = next;
-        fibArray[counter+2] = next;
+    //array_fib will be filled starting with the first position (zero)
+    int i = 0;
+    for (int value = ini; value <= end; value++) {
+        array_fib[i] = fib(value);
+        i++;
     }
-    return fibArray;
+    return array_fib;
 }
 
 int main(){
     long double *p;
     long double initial, final;
-    printf("Type two numbers (starting from 1) separated by a space: ");
+    printf("Type two numbers starting from 1 (fibonacci range) separated by a space: ");
     scanf("%Lf %Lf", &initial, &final);
-    p = fibonacci(final+1);
-    for (int i = initial-1; i < final; i++) {
-        printf("Element %d - %.0Lf\n", i+1, p[i]);
+    
+    p = fibonacci(initial-1, final-1);
+    for (int i = 0; i <= (final-initial); i++) {
+        printf("%.0Lf\n", p[i]);
     }
 }
